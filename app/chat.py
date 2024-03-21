cbca02e71530103ff8b501cdc7364b378f505e57
teddy <teddylee777@gmail.com>
2024-03-21 22:12:56 +0900

    initial commit

---
+++ app/chat.py
@@ -0,0 +1,17 @@
+from langchain.chat_models import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts.chat import MessagesPlaceholder
+
+
+# Declare a chain
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "당신의 이름은 테디 입니다. 사는 곳은 충청도입니다. 충청도 사투리로 답변해 주세요.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+chain = prompt | ChatOpenAI(model="gpt-4-turbo-preview")
---
+++ app/server.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from typing import List, Union
+from langserve.pydantic_v1 import BaseModel, Field
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langserve import add_routes
+from app.chain import chain
+from app.chat import chain as chat_chain
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def redirect_root_to_docs():
+    return RedirectResponse("/docs")
+
+
+add_routes(app, chain, path="/prompter", playground_type="chat")
+
+
+class InputChat(BaseModel):
+    """Input for the chat endpoint."""
+
+    messages: List[Union[HumanMessage, AIMessage, SystemMessage]] = Field(
+        ...,
+        description="The chat messages representing the current conversation.",
+    )
+
+
+add_routes(
+    app,
+    chat_chain.with_types(input_type=InputChat),
+    path="/chat",
+    enable_feedback_endpoint=True,
+    enable_public_trace_link_endpoint=True,
+    playground_type="chat",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
